b2b981d98124f0dafef3dd2489c17bd7f8d940ca
Guilherme jeske <guilhermeieski@gmail.com>
2026-03-02 18:49:36 -0300

    add comando npx puppeteer browsers install chrome

--- src/whatsapp/client.js
+++ src/whatsapp/client.js
@@ -25,7 +25,13 @@ function initializeWhatsApp() {
     }),
     puppeteer: {
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+      ],
     },
   });
 
